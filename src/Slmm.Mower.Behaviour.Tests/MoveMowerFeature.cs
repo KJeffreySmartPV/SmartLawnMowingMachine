9e681516806201ec9282a14fe3427c9ade76ebbd
Kieran Jeffrey-Smart <kjeffreysmart@pivotal.io>
2020-06-23 13:11:24 +0100

    Initial commit

---
+++ src/Slmm.Mower.Behaviour.Tests/MoveMowerFeature.cs
@@ -0,0 +1,112 @@
+﻿namespace Slmm.Mower.Behaviour.Tests
+{
+    using FluentAssertions;
+    using System;
+    using slmm.LawnMowing.Exceptions;
+    using slmm.LawnMowing.Model;
+    using Xbehave;
+
+    public class MoveMowerFeature
+    {
+        public class MoveMowerTestContext
+        {
+            public Garden Garden { get; internal set; }
+            public Mower Mower { get; internal set; }
+        }
+
+        [Scenario]
+        [Example(2, 2, 1, 1, Orientation.South)]
+        [Example(2, 2, 2, 2, Orientation.South)]
+        public void GetPositionFromStartup(int gardenLength, int gardenWidth, int startX, int startY, Orientation orientation)
+        {
+            var context = new MoveMowerTestContext();
+            Position currentPosition = null;
+            "Given I have a garden"
+                .x(() => context.Garden = new Garden(gardenLength, gardenWidth));
+            "And I have started a new Mower inside the garden"
+                .x(() => context.Mower = this.CreateNewMower(context.Garden, startX, startY, orientation));
+            "When I get the Mower's position"
+                .x(() => currentPosition = context.Mower.GetPosition());
+            "Then it should be as I set it"
+                .x(() => this.AssertPosition(currentPosition, startX, startY, orientation));
+        }
+
+        [Scenario]
+        [Example(2, 2, 1, 1, Orientation.South, 1, 2)]
+        [Example(2, 2, 1, 1, Orientation.East, 2, 1)]
+        [Example(2, 2, 2, 2, Orientation.North, 2, 1)]
+        [Example(2, 2, 2, 2, Orientation.West, 1, 2)]
+        [Example(5, 5, 2, 2, Orientation.South, 2, 3)]
+        public void MoveMowerToAValidCell(int gardenLength, int gardenWidth, int startX, int startY, Orientation orientation, int expectedX, int expectedY)
+        {
+            var context = new MoveMowerTestContext();
+            "Given I have a garden"
+                .x(() => context.Garden = new Garden(gardenLength, gardenWidth));
+            "And I have started a new Mower inside the garden"
+                .x(() => context.Mower = this.CreateNewMower(context.Garden, startX, startY, orientation));
+            "When I move the Mower forward"
+                .x(() => context.Mower.Move());
+            "Then its position should change as expected"
+                .x(() => this.AssertPosition(context.Mower.GetPosition(), expectedX, expectedY, orientation));
+        }
+
+        [Scenario]
+        [Example(2, 2, 1, 1, Orientation.North)]
+        [Example(2, 2, 1, 1, Orientation.West)]
+        [Example(2, 2, 2, 2, Orientation.South)]
+        [Example(2, 2, 2, 2, Orientation.East)]
+        public void MoveMowerToAnInvalidCell(int gardenLength, int gardenWidth, int startX, int startY, Orientation orientation)
+        {
+            var context = new MoveMowerTestContext();
+            Action moveAction = null;
+
+            Position currentPosition = new Position(new Coordinates(startX, startY), orientation);
+            "Given I have a garden"
+                .x(() => context.Garden = new Garden(gardenLength, gardenWidth));
+            "And I have started a new Mower inside the garden"
+                .x(() => context.Mower = this.CreateNewMower(context.Garden, startX, startY, orientation));
+            "When I move the Mower forward"
+                .x(() => moveAction = new Action(() => context.Mower.Move()));
+            "Then its position should not change"
+                .x(() => moveAction.Should().Throw<OutOfGardenBoundaryException>());
+        }
+
+        [Scenario]
+        [Example(2, 2, 1, 1, Orientation.North, TurnDirection.Clockwise, Orientation.East)]
+        [Example(2, 2, 1, 1, Orientation.West, TurnDirection.Clockwise, Orientation.North)]
+        [Example(2, 2, 2, 2, Orientation.South, TurnDirection.Clockwise, Orientation.West)]
+        [Example(2, 2, 2, 2, Orientation.East, TurnDirection.Clockwise, Orientation.South)]
+        [Example(2, 2, 1, 1, Orientation.North, TurnDirection.AntiClockwise, Orientation.West)]
+        [Example(2, 2, 1, 1, Orientation.West, TurnDirection.AntiClockwise, Orientation.South)]
+        [Example(2, 2, 2, 2, Orientation.South, TurnDirection.AntiClockwise, Orientation.East)]
+        [Example(2, 2, 2, 2, Orientation.East, TurnDirection.AntiClockwise, Orientation.North)]
+        public void TurnMower(int gardenLength, int gardenWidth, int startX, int startY, Orientation orientation, TurnDirection turnDirection, Orientation expectedOrientation)
+        {
+            var context = new MoveMowerTestContext();
+            Position currentPosition = new Position(new Coordinates(startX, startY), orientation);
+            "Given I have a garden"
+                .x(() => context.Garden = new Garden(gardenLength, gardenWidth));
+            "And I have started a new Mower inside the garden"
+                .x(() => context.Mower = this.CreateNewMower(context.Garden, startX, startY, orientation));
+            "When I turn the Mower"
+                .x(() => context.Mower.Turn(turnDirection));
+            "Then its orientation should be as expected"
+                .x(() => context.Mower.GetPosition().Orientation.Should().Be(expectedOrientation));
+        }
+
+        private void AssertPosition(Position currentPosition, int posX, int posY, Orientation orientation)
+        {
+            currentPosition.Should().NotBeNull();
+            currentPosition.Coordinates.X.Should().Be(posX);
+            currentPosition.Coordinates.Y.Should().Be(posY);
+            currentPosition.Orientation.Should().Be(orientation);
+        }
+
+        private Mower CreateNewMower(Garden garden, int startX, int startY, Orientation orientation)
+        {
+            var newMower = new Mower(garden);
+            newMower.Start(new Position(new Coordinates(startX, startY), orientation));
+            return newMower;
+        }
+    }
+}
---
+++ src/Slmm.Mower.Behaviour.Tests/StartupMowerFeature.cs
@@ -0,0 +1,78 @@
+﻿namespace Slmm.Mower.Behaviour.Tests
+{
+    using Xbehave;
+    using FluentAssertions;
+    using slmm.LawnMowing.Model;
+
+    public class StartupMowerFeature
+    {
+        public class StartupTestContext
+        {
+            public Garden Garden { get; internal set; }
+            public Position StartingPosition { get; internal set; }
+            public Mower Mower { get; internal set; }
+        }
+                
+        [Scenario]
+        [Example(2, 2, 1, 1)]
+        [Example(2, 2, 2, 2)]
+        public void CreateAMower(int gardenLength, int gardenWidth, int startX, int startY)
+        {
+            var context = new StartupTestContext();
+
+            "Given I have a garden"
+                .x(() => context.Garden = new Garden(gardenLength, gardenWidth));
+            "And I have a Mower"
+                .x(() => context.Mower = new Mower(context.Garden));
+            "And I have a position I want the mower to start in"
+                .x(() => context.StartingPosition = new Position(new Coordinates(startX, startY), Orientation.South));
+            "When I start the Mower"
+                .x(() => context.Mower.Start(context.StartingPosition));
+            "Then the Mower starts sucesfully"
+                .x(() => context.Mower.HasStarted.Should().BeTrue());
+        }
+
+        [Scenario]
+        [Example(2, 2, 0, 0)]
+        [Example(2, 2, 0, 1)]
+        [Example(2, 2, 1, 0)]
+        [Example(2, 2, 3, 1)]
+        [Example(2, 2, 1, 3)]
+        [Example(2, 2, 3, 3)]
+        [Example(5, 5, 6, 6)]
+        public void CreateAMowerOutsideTheGarden(int gardenLength, int gardenWidth, int startX, int startY)
+        {
+            var context = new StartupTestContext();
+
+            "Given I have a garden"
+                .x(() => context.Garden = new Garden(gardenLength, gardenWidth));
+            "And I have a Mower"
+                .x(() => context.Mower = new Mower(context.Garden));
+            "And I have a position I want the mower to start in"
+                .x(() => context.StartingPosition = new Position(new Coordinates(startX, startY), Orientation.South));
+            "When I start the Mower"
+                .x(() => context.Mower.Start(context.StartingPosition));
+            "Then the Mower should not start"
+                .x(() => context.Mower.HasStarted.Should().BeFalse());
+        }
+
+        [Scenario]
+        public void StartAMowerThatHasAlreadyBeenStarted()
+        {
+            var context = new StartupTestContext();
+
+            "Given I have a garden"
+                .x(() => context.Garden = new Garden(1, 1));
+            "And I have a Mower"
+                .x(() => context.Mower = new Mower(context.Garden));
+            "And I have a position I want the mower to start in"
+                .x(() => context.StartingPosition = new Position(new Coordinates(1, 1), Orientation.South));
+            "And I have started the Mower"
+                .x(() => context.Mower.Start(context.StartingPosition));
+            "When I start the Mower again"
+                .x(() => context.Mower.Start(context.StartingPosition));
+            "Then the Mower should not start"
+                .x(() => context.Mower.HasStarted.Should().BeTrue());
+        }
+    }
+}
